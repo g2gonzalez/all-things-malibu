59e125f3c63c843dd0cf1034a4a904f945b17458
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-07-13 17:37:24 -0700

    Added user and listing validations with error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
-   has_secure_password
    has_many :listings
+   validates :username, :email, presence: true
+   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+   has_secure_password
 
 end
