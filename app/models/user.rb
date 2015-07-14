9f557badb4aa4e3347fd70d18d2b2fb07289747b
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-07-13 18:08:32 -0700

    Rearranged user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-   has_many :listings
    validates :username, :email, presence: true
    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
    has_secure_password
+
+   has_many :listings
 
 end
