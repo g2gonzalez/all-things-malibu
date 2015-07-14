ce4ecf2e8edfb5cc82fde8f8cba28121ff5bef31
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-07-13 18:20:23 -0700

    Updated user and listing models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,10 @@
 class Listing < ActiveRecord::Base
 
 	mount_uploader :listing_img, ListingImgUploader
-	belongs_to :user
 	validates_presence_of :listing_img
 	validates :name, :description, :price, :quantity, presence: true
   	validates :price, :quantity, numericality: { greater_than: 0 }
+
+  	belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
    has_secure_password
 
-   has_many :listings
+   has_many :listings, dependent: :destroy
 
 end
